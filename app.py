8b9713135585e758f371657e4da92929c6e851d2
HorvLala <horvathwebdev@gmail.com>
2022-12-14 22:46:35 +0100

    Az első commitálása az Flask_one projectnek

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post 1. LAlalalalalalala.',
+        'author': 'Lala'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2. LAlalalalalalala.'
+    },
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
+
+@app.route('/home')
+def hello():
+    return 'Hello home2'
+
+@app.route('/onlyget', methods=['GET'])
+def get_req():
+    return 'You can only get this webpage.'
+
+if __name__ == "__main__":
+    app.run(debug=True)

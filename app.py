4cf6886628f28cf33951a4659ea349c87a44f75a
HorvLala <horvathwebdev@gmail.com>
2022-12-19 16:33:49 +0100

    delete-edit

--- app.py
+++ app.py
@@ -37,13 +37,37 @@ def posts():
     if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = BlogPost(title=post_title,content=post_content, author='Lala')
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title,content=post_content, author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
     else:
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
+    
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET','POST'])
+def edit(id):
+    
+    post = BlogPost.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+        
+
 
 @app.route('/home')
 def hello():
